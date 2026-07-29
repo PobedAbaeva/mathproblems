f5cd2c011e2b38d1adb59e93e81ecdaa7aa1e510
vlad-2011dmitriev <vlad-2011dmitriev@yandex.ru>
2026-07-29 19:21:16 +0300

    сделаны шаблоны register и login

--- main.py
+++ main.py
@@ -11,5 +11,14 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form.get("nickname")
+        password = request.form.get("password")
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Аварийный доступ')
+
+
 if __name__ == "__main__":
     app.run(port=5000)
