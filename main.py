a477d4ad438113d203d26ce9564f6968e63f15f1
vlad-2011dmitriev <vlad-2011dmitriev@yandex.ru>
2026-07-29 16:12:03 +0300

    сделан черновик main и первые шаблоны

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
- 
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session, flash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
