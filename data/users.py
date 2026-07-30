745b7975db0799d38dc34b6e7f7cb58edefba3af
vlad-2011dmitriev <vlad-2011dmitriev@yandex.ru>
2026-07-30 11:47:10 +0300

    users редактирован

--- data/users.py
+++ data/users.py
@@ -1,4 +1,3 @@
-import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 
@@ -9,9 +8,6 @@ class User(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer, 
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String, 
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime, 
-                                     default=datetime.datetime.now)
